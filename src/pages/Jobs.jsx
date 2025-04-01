ae032463b22e700874cee1e3a23377760715c994
DarcMattz <johnvirgilcarvajal@gmail.com>
2025-04-01 16:23:53 +0800

    feat: Update Jobs page layout to include JobList component and job statistics

--- src/pages/Jobs.jsx
+++ src/pages/Jobs.jsx
@@ -1,11 +1,43 @@
-import FixingBugs from '../assets/fixing-bugs.svg';
+import JobList from "../layouts/JobList";
+
 
 const Jobs = () => {
     return (
-        <div className="p-6 text-center text-lg font-semibold">
-            <img src={FixingBugs} alt="Fixing Bugs" className="max-w-md mx-auto" />
+        <div className="flex-col items-center justify-center">
+            <section class="m-10 grid grid-cols-4 grid-rows-[7rem] gap-10">
+                <div
+                    onClick={() => alert("applicants")}
+                    class="rounded-md grid place-content-center cursor-pointer bg-white border border-gray-light"
+                >
+                    <span class="text-3xl text-center ">147</span>
+                    <div class="text-sm text-gray-500 text-center">Applications</div>
+                </div>
+                <div
+                    onClick={() => alert("industry")}
+                    class="rounded-md grid place-content-center cursor-pointer bg-white border border-gray-light"
+                >
+                    <span class="text-3xl text-center">4</span>
+                    <div class="text-sm text-gray-500 text-center">Industries</div>
+                </div>
+                <div
+                    onClick={() => alert("open")}
+                    class="rounded-md grid place-content-center cursor-pointer bg-white border border-gray-light"
+                >
+                    <span class="text-2xl text-center">5</span>
+                    <div class="text-sm text-gray-500 text-center">Open Jobs</div>
+                </div>
+                <div
+                    onClick={() => alert("close")}
+                    class="rounded-md grid place-content-center cursor-pointer bg-white border border-gray-light"
+                >
+                    <span class="text-2xl text-center">7</span>
+                    <div class="text-sm text-gray-500 text-center">Closed Jobs</div>
+                </div>
+            </section>
+            <JobList />
         </div>
     );
-}
+};
+
 
 export default Jobs;
