7da436c3017e1bdf33df5dacdc7dd83f9d24235e
DarcMattz <johnvirgilcarvajal@gmail.com>
2025-04-01 16:23:57 +0800

    refactor: Simplify Jobs component rendering in Listings page

--- src/pages/Listings.jsx
+++ src/pages/Listings.jsx
@@ -170,9 +170,9 @@ export default function Listings() {
         );
       case "jobs":
         return (
-          <div className="flex h-full items-center justify-center">
-            <Jobs />
-          </div>
+
+          <Jobs />
+
         );
       case "config":
         return (
