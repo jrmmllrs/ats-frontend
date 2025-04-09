599759f2e69f858aa0a5ea02103efc2ed932eb1c
DarcMattz <johnvirgilcarvajal@gmail.com>
2025-04-09 08:50:25 +0800

    feat: Implement tab functionality for job listings with improved UI

--- src/pages/Jobs.jsx
+++ src/pages/Jobs.jsx
@@ -1,13 +1,31 @@
 import JobList from "../layouts/JobList";
 import JobCountStore from "../context/jobsCountStore";
-import { useEffect } from "react";
-import { fetchCloseJobsCount, fetchIndustriesCount, fetchOpenJobsCount, getCloseJobs, getOpenJobs } from "../utils/jobListing";
+import { useEffect, useState } from "react";
+import { fetchCloseJobsCount, fetchIndustriesCount, fetchJobs, fetchOpenJobsCount, getCloseJobs, getOpenJobs } from "../utils/jobListing";
 import jobStore from "../context/jobListingStore";
 
 
 const Jobs = () => {
     const { industriesCount, setIndustriesCount, openJobsCount, setOpenJobsCount, closeJobsCount, setCloseJobsCount } = JobCountStore();
     const { setIsGearModalOpen, setJobsData } = jobStore();
+    const [activeTab, setActiveTab] = useState();
+
+    const handleTabClick = (tab) => {
+        console.log("tab", tab);
+
+        if (tab === "open" && activeTab !== "open") {
+            setActiveTab("open");
+            getOpenJobs(setJobsData);
+        } else if (tab === "close" && activeTab !== "close") {
+            setActiveTab("close");
+            getCloseJobs(setJobsData);
+        } else {
+            setActiveTab(null);
+            fetchJobs(setJobsData);
+        }
+    }
+
+
 
     useEffect(() => {
         const getCounts = async () => {
@@ -30,21 +48,21 @@ const Jobs = () => {
                 </div> */}
                 <div
                     onClick={() => setIsGearModalOpen(true)}      //nyenyenyenyenyenye
-                    className="rounded-md grid place-content-center cursor-pointer bg-white border border-gray-light"
+                    className="rounded-2xl grid place-content-center cursor-pointer bg-white border border-gray-light"
                 >
                     <span className="text-3xl text-center">{industriesCount}</span>
                     <div className="text-sm text-gray-500 text-center">Industries</div>
                 </div>
                 <div
-                    onClick={() => getOpenJobs(setJobsData)}
-                    className="rounded-md grid place-content-center cursor-pointer bg-white border border-gray-light"
+                    onClick={() => handleTabClick("open")}
+                    className={`rounded-2xl grid place-content-center cursor-pointer bg-white border border-gray-light ${activeTab === "open" ? "border-teal-soft border-2" : ""}`}
                 >
                     <span className="text-3xl text-center">{openJobsCount}</span>
                     <div className="text-sm text-gray-500 text-center">Open Jobs</div>
                 </div>
                 <div
-                    onClick={() => getCloseJobs(setJobsData)}
-                    className="rounded-md grid place-content-center cursor-pointer bg-white border border-gray-light"
+                    onClick={() => handleTabClick("close")}
+                    className={`rounded-2xl grid place-content-center cursor-pointer bg-white border border-gray-light ${activeTab === "close" ? "border-teal-soft border-2" : ""}`}
                 >
                     <span className="text-3xl text-center">{closeJobsCount}</span>
                     <div className="text-sm text-gray-500 text-center">Closed Jobs</div>
