2fd729fdfa3a2af29c3a93d64cf42017e1f301a9
DarcMattz <johnvirgilcarvajal@gmail.com>
2025-04-10 13:36:07 +0800

    feat: Update MessageBox component to use a local profile image asset

--- src/components/MessageBox.jsx
+++ src/components/MessageBox.jsx
@@ -1,3 +1,4 @@
+import profileUser from "../assets/profile-user.png";
 
 export default function MessageBox({ message, sender, date }) {
     return (
@@ -6,13 +7,15 @@ export default function MessageBox({ message, sender, date }) {
         <div className="my-4 border border-gray-light rounded-lg p-6">
 
             <div className="mb-3 flex items-center gap-3">
-                <div className="relative h-10 w-10 overflow-hidden rounded-full bg-gray-200">
+                <div className="relative h-10 w-10 overflow-hidden rounded-full bg-white">
                     <img
-                        src="https://i.pinimg.com/originals/ee/5a/b2/ee5ab2fdab2c09e8995e0e5225a61d8f.gif"
-                        alt="Marvin Bautista"
-                        className="h-full w-full object-cover"
+                        src={profileUser}
+                        alt="User Profile"
+                        className="h-full w-full rounded-full object-cover"
                     />
                 </div>
+
+
                 <div className="flex-col">
                     <h3 className="headline text-gray-dark">{sender}</h3>
                     <p className="body-tiny text-gray-400">{date}</p>
