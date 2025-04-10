7a4194e3d843bbb777f5b0e196eb5f9756de47ac
DarcMattz <johnvirgilcarvajal@gmail.com>
2025-04-10 09:07:22 +0800

    refactor: Simplify layout structure in Configurations component

--- src/pages/Configurations.jsx
+++ src/pages/Configurations.jsx
@@ -6,10 +6,8 @@ const Configurations = () => {
         <div className="container mx-auto px-4 py-10">
             <div className="flex flex-col items-center text-center mb-10">
 
-
-
             </div>
-            <div className="flex w-full max-w-4xl justify-center mx-auto max-w-5xl gap-8">
+            <div className="flex w-full justify-center mx-auto max-w-5xl gap-8">
                 <SMTPConfiguration />
                 <GdriveConfig />
             </div>
