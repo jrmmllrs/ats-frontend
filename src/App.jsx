657d86b07f08ca3e8ed8711907f7f4738749e0de
Abakus <yacapinton@gmail.com>
2025-04-22 14:02:45 +0800

    feat: Add User Management link and route in Sidebar and App components

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import ApplicantListView from "./pages/ApplicantListView";
 import AnalysisPage from "./pages/AnalysisPage";
 import Jobs from "./pages/Jobs";
 import Configurations from "./pages/Configurations";
+import UserManagementPage from "./pages/UserManagementPage";
 
 function App() {
   return (
@@ -29,6 +30,7 @@ function App() {
           <Route index element={<Navigate to="/dashboard" />} />
 
           {/* Main views as separate routes */}
+          <Route path="usermanagement" element={<UserManagementPage />} />
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="applicants" element={<ApplicantListView />} />
           <Route path="applicants/:id" element={<ApplicantListView />} />
